885f77b255579149f6b0d4fbdad372ae54a862c8
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-25 15:43:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const makeChange = (c) => {
   // your name here
+	const q = Math.floor(c/25);
+	c = c%25;
+	const d = Math.floor(c/10);
+	c = c%10;
+	const n = Math.floor(c/5);
+	c = c%5;
+	const p = c;
+	return { "q": q, "d": d, "n": n, "p": p};
 };
 
 // Do not the change the code below
